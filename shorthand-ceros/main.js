5373e01130d6ee09243a57caeb5acff3250076de
Andrew Georgiou <andrew.georgiou@ft.com>
2017-07-18 15:44:33 +0100

    Adding support for ceros

--- shorthand-ceros/main.js
+++ shorthand-ceros/main.js
@@ -8,6 +8,7 @@ const extname = require('path').extname;
 const S3 = require('aws-sdk').S3;
 const cheerio = require('cheerio');
 const comments = require('./comments');
+const headerFooter = require('./header-footer');
 const imageservice = require('./imageservice');
 const relPathCssJs = require('./rel-path-css-js');
 const oTracking = require('./o-tracking');
@@ -32,7 +33,8 @@ function pipeline(body, item, cb) {
 
   if (args.uuid) { // Editorial project
     const withComments = comments($, args);
-    const withTracking = oTracking(withComments, args);
+    const withHeader = headerFooter(withComments, args);
+    const withTracking = oTracking(withHeader, args);
     // rest of editorial pipeline...
     utils.deploy(item, withTracking.html())
       .then(key => {
