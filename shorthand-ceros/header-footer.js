f891ec9b05621896531f155e981efb86a2a66656
Andrew Georgiou <andrew.georgiou@ft.com>
2017-07-26 17:25:32 +0100

    adding support for ceros

--- shorthand-ceros/header-footer.js
+++ shorthand-ceros/header-footer.js
@@ -187,10 +187,10 @@ module.exports = $ => {
   $('head').prepend(headSnippet);
   $('body').prepend(headerSnippet);
   $('body').append(footer);
-  let sponsor = ( $("meta[name=author]") && $("meta[name=author]").attr("content") ) ? $("meta[name=author]").attr("content").split(',') : null;
+  let sponsor = ($('meta[name=author]') && $('meta[name=author]').attr('content')) ? $('meta[name=author]').attr('content').split(',') : null;
   if (sponsor) {
-    $('disclaimer__sponsor-name').innerHTML = sponsor;
-    }
+    $('disclaimer__sponsor-name').innerHTML = sponsor[0];
+  }
 
   return $;
 };
